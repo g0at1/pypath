0b3b228192f61737e79aee653e4b1c0c78006178
Michał Lendzion <95431893+g0at1@users.noreply.github.com>
2025-06-08 19:58:19 +0200

    Show git branch in command mode (#5)
    * Show git branch in command mode
    
    * Add cache to store branches

--- main.py
+++ main.py
@@ -7,6 +7,25 @@ import grp
 import time
 import subprocess
 import re
+from git import Repo
+from git.exc import InvalidGitRepositoryError
+
+_cached_path = None
+_cached_branch = None
+
+
+def get_branch_for_path(path):
+    global _cached_path, _cached_branch
+
+    if path != _cached_path:
+        _cached_path = path
+        try:
+            repo = Repo(path, search_parent_directories=True)
+            _cached_branch = repo.active_branch.name
+        except (InvalidGitRepositoryError, TypeError):
+            _cached_branch = None
+
+    return _cached_branch
 
 
 def update_selection_from_regex(buffer, entries):
@@ -361,6 +380,12 @@ def draw_directory(stdscr, current_path, selected, command_mode=False, cmd_buffe
             stdscr.addstr(screen_y, prefix_len, part_name[:remaining], attr_name)
 
     if command_mode:
+        branch = get_branch_for_path(current_path)
+        if branch:
+            branch_line = f"Branch: {branch}"
+            stdscr.move(h - 3, 0)
+            stdscr.clrtoeol()
+            stdscr.addstr(h - 3, 0, branch_line.ljust(w - 1), curses.A_BOLD)
         pwd_line = f"PWD: {current_path}"
         stdscr.addstr(h - 2, 0, pwd_line[: w - 1])
 
